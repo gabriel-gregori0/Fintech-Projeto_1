2a802763ac80eae7f4c143d8dd8df47e578dc8cd
gabr1eldev <gabriel.gregorio1987@gmail.com>
2025-06-12 21:46:35 -0300

    Creating column 2 and all links

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,43 @@
 
     for (let i = 0; i < 3; i++) {
         const col = document.createElement('div');
-        col.className = "col-4";
-        const p = document.createElement('p');
-        col.classList.add(`column-${i + 1}`);
+        col.classList.add(`column-${i + 1}`,"col-4");
 
         row.appendChild(col);
     }
     container.appendChild(row);
     document.body.appendChild(container);
 })();
+
+const column1 = () => {
+    const col1 = document.querySelector('.column-1');
+    const label = document.createElement('label');
+    label.classList.add('balance',"text-center", "justify-content-center","d-flex");
+    label.innerText = 'Balance';
+
+    col1.appendChild(label);
+}
+
+const column2 = () => {
+    const col = document.querySelector('.column-2');
+    const row2 = document.createElement('div');
+    col.classList.add("row", "align-items-center");
+    col.appendChild(row2);
+    const col2 = document.createElement('div');
+    col2.classList.add("col-12", "d-flex", "flex-column", "align-items-center");
+    row2.appendChild(col2);
+    
+    const aClass = ["expense","investiments","add","withdraw"];
+    const aTexts = ["Expense", "Investments", "Add", "Withdraw"];
+
+    for (let i = 0; i < 4; i++) {
+        const a = document.createElement('a');
+        a.classList.add(aClass[i], "text-center", "justify-content-center", "d-flex");
+        a.innerText = aTexts[i];
+
+        col2.appendChild(a);
+    }
+}
+
+column1();
+column2();
